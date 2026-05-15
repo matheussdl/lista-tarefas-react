daf0b23fdee9610ff9597c99da85b2c0ed002285
Matheus Dias <matheus.tthentic@gmail.com>
2026-05-15 00:45:59 -0300

    Projeto inicial

---
+++ App.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import Task from "./components/Task";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+  console.log("Lista atualizada:", tasks);
+}, [tasks]);
+
+  function addTask() {
+    setTasks([...tasks, input]);
+    setInput("");
+  }
+
+  return (
+    <div>
+      <h1>Lista de Tarefas</h1>
+
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <button onClick={addTask}>
+        Adicionar
+      </button>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <Task key={index} text={task} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ components/Task/index.jsx
@@ -0,0 +1,5 @@
+function Task(props) {
+  return <li>{props.text}</li>;
+}
+
+export default Task;
